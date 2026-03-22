c19a0de7a6ac1596d9058da161797146296bdd44
TUF <TUF@DESKTOP-FKUVJD9>
2026-03-22 14:18:03 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace autoclick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBoxButton;
+        private System.Windows.Forms.Timer timer1;
+        
+        
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            buttonStart = new Button();
+            buttonStop = new Button();
+            numericUpDown1 = new NumericUpDown();
+            comboBoxButton = new ComboBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            checkBoxHold = new CheckBox();
+            labelCPS = new Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            buttonStart.ForeColor = Color.Red;
+            buttonStart.Location = new Point(3, 220);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(100, 40);
+            buttonStart.TabIndex = 0;
+            buttonStart.Text = "START (F6)";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // buttonStop
+            // 
+            buttonStop.ForeColor = Color.Red;
+            buttonStop.Location = new Point(109, 220);
+            buttonStop.Name = "buttonStop";
+            buttonStop.Size = new Size(100, 40);
+            buttonStop.TabIndex = 1;
+            buttonStop.Text = "STOP (F6)";
+            buttonStop.UseVisualStyleBackColor = true;
+            buttonStop.Click += buttonStop_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.ForeColor = Color.Red;
+            numericUpDown1.Location = new Point(273, 201);
+            numericUpDown1.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 2;
+            numericUpDown1.Value = new decimal(new int[] { 50, 0, 0, 0 });
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // comboBoxButton
+            // 
+            comboBoxButton.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxButton.ForeColor = Color.Red;
+            comboBoxButton.FormattingEnabled = true;
+            comboBoxButton.Location = new Point(243, 230);
+            comboBoxButton.Name = "comboBoxButton";
+            comboBoxButton.Size = new Size(150, 23);
+            comboBoxButton.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // checkBoxHold
+            // 
+            checkBoxHold.AutoSize = true;
+            checkBoxHold.ForeColor = Color.Red;
+            checkBoxHold.Location = new Point(61, 199);
+            checkBoxHold.Name = "checkBoxHold";
+            checkBoxHold.Size = new Size(110, 19);
+            checkBoxHold.TabIndex = 4;
+            checkBoxHold.Text = "Maintenir le clic";
+            checkBoxHold.UseVisualStyleBackColor = true;
+            // 
+            // labelCPS
+            // 
+            labelCPS.AutoSize = true;
+            labelCPS.Font = new Font("Microsoft Sans Serif", 8.25F);
+            labelCPS.ForeColor = Color.Red;
+            labelCPS.Location = new Point(12, 201);
+            labelCPS.Name = "labelCPS";
+            labelCPS.Size = new Size(43, 13);
+            labelCPS.TabIndex = 5;
+            labelCPS.Text = "CPS : 0";
+            labelCPS.Click += labelCPS_Click;
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(405, 265);
+            Controls.Add(labelCPS);
+            Controls.Add(checkBoxHold);
+            Controls.Add(buttonStart);
+            Controls.Add(buttonStop);
+            Controls.Add(numericUpDown1);
+            Controls.Add(comboBoxButton);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            KeyPreview = true;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "AutoClicker X";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox checkBoxHold;
+        private Label labelCPS;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace autoclick
+{
+    public partial class Form1 : Form
+    {
+        // Timer clic automatique
+        private System.Windows.Forms.Timer timer1;
+
+        // Timer animation CPS
+        private System.Windows.Forms.Timer animationTimer;
+
+        // Glow CPS
+        private int glow = 0;
+        private bool glowUp = true;
+
+        // CPS compteur
+        private int clickCount = 0;
+        private DateTime lastTime;
+
+        // Pour le clic souris
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        const int LEFTDOWN = 0x02;
+        const int LEFTUP = 0x04;
+        const int RIGHTDOWN = 0x08;
+        const int RIGHTUP = 0x10;
+
+        // Hotkey F6 global
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, Keys vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        const int HOTKEY_ID = 9000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lastTime = DateTime.Now;
+
+            // 🔴 Design rouge gaming
+            this.BackColor = Color.FromArgb(18, 18, 18);
+
+            // Boutons START/STOP
+            buttonStart.BackColor = Color.FromArgb(200, 30, 30);
+            buttonStart.ForeColor = Color.White;
+            buttonStart.FlatStyle = FlatStyle.Flat;
+            buttonStart.FlatAppearance.BorderSize = 0;
+
+            buttonStop.BackColor = Color.FromArgb(120, 20, 20);
+            buttonStop.ForeColor = Color.White;
+            buttonStop.FlatStyle = FlatStyle.Flat;
+            buttonStop.FlatAppearance.BorderSize = 0;
+
+            // ComboBox
+            comboBoxButton.BackColor = Color.FromArgb(30, 30, 30);
+            comboBoxButton.ForeColor = Color.White;
+
+            // NumericUpDown
+            numericUpDown1.BackColor = Color.FromArgb(30, 30, 30);
+            numericUpDown1.ForeColor = Color.White;
+
+            // Label CPS
+            labelCPS.ForeColor = Color.Red;
+            labelCPS.Font = new Font("Segoe UI", 18, FontStyle.Bold);
+
+            // Setup ComboBox clic gauche/droit
+            comboBoxButton.Items.Add("Gauche");
+            comboBoxButton.Items.Add("Droite");
+            comboBoxButton.SelectedIndex = 0;
+
+            // Timer clic automatique
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += timer1_Tick;
+
+            // Timer animation CPS
+            animationTimer = new Timer();
+            animationTimer.Interval = 40;
+            animationTimer.Tick += AnimateGlow;
+            animationTimer.Start();
+
+            // Hover boutons
+            buttonStart.MouseEnter += ButtonHover;
+            buttonStart.MouseLeave += ButtonLeave;
+            buttonStop.MouseEnter += ButtonHover;
+            buttonStop.MouseLeave += ButtonLeave;
+
+            // Hotkey F6
+            RegisterHotKey(this.Handle, HOTKEY_ID, 0, Keys.F6);
+            this.FormClosing += Form1_FormClosing;
+        }
+
+        // Bouton START
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)numericUpDown1.Value;
+            timer1.Start();
+        }
+
+        // Bouton STOP
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        // Timer clic automatique
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (comboBoxButton.SelectedItem.ToString() == "Gauche")
+            {
+                mouse_event(LEFTDOWN, 0, 0, 0, 0);
+                mouse_event(LEFTUP, 0, 0, 0, 0);
+            }
+            else
+            {
+                mouse_event(RIGHTDOWN, 0, 0, 0, 0);
+                mouse_event(RIGHTUP, 0, 0, 0, 0);
+            }
+
+            clickCount++;
+            if ((DateTime.Now - lastTime).TotalSeconds >= 1)
+            {
+                labelCPS.Text = "CPS : " + clickCount;
+                clickCount = 0;
+                lastTime = DateTime.Now;
+            }
+        }
+
+        // Changer vitesse
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = (int)numericUpDown1.Value;
+        }
+
+        // Animation glow CPS
+        private void AnimateGlow(object sender, EventArgs e)
+        {
+            if (glowUp) glow += 5; else glow -= 5;
+            if (glow >= 80) glowUp = false;
+            if (glow <= 0) glowUp = true;
+            labelCPS.ForeColor = Color.FromArgb(255, 200, glow, glow);
+        }
+
+        // Hover boutons
+        private void ButtonHover(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn != null) btn.BackColor = Color.FromArgb(255, 50, 50);
+        }
+
+        private void ButtonLeave(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                if (btn == buttonStart) btn.BackColor = Color.FromArgb(200, 30, 30);
+                else btn.BackColor = Color.FromArgb(120, 20, 20);
+            }
+        }
+
+        // Hotkey F6
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID)
+                timer1.Enabled = !timer1.Enabled;
+            base.WndProc(ref m);
+        }
+
+        // Désenregistrer hotkey
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID);
+        }
+    }
+}
